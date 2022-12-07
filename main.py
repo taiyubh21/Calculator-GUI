ce90387a20184c3e25363b639fa1b1ea9f2c16db
Mohammad Taiyub Hussain <taiyubhussain@gmail.com>
2022-12-07 00:03:51 +0000

    Added two functions: First function allows buttons to return number values to display textbox Second function allows A/C button to clear display textbox

--- main.py
+++ main.py
@@ -3,6 +3,20 @@
 # get everything from tkinter import
 from tkinter import *
 
+
+# function created for return value of number to textbox
+def value_return(number):
+    # insert value into text box
+    # allows value not to be overwritten so can write big number using multiple number buttons
+    display.insert(END, number)
+
+
+# function created to clear textbox using A/C button
+def clear_value():
+    # deletes whatever is in the textbox
+    display.delete(0, END)
+
+
 if __name__ == "__main__":
     # create gui
     window = Tk()
@@ -16,22 +30,24 @@ if __name__ == "__main__":
     display.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
     # creating buttons
-    button0 = Button(window, text="0", padx=33, pady=22, font=("Arial", 15))
+    # use value_return function and pass number through in the specific buttons
+    button0 = Button(window, text="0", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(0))
 
-    button1 = Button(window, text="1", padx=33, pady=22, font=("Arial", 15))
-    button2 = Button(window, text="2", padx=33, pady=22, font=("Arial", 15))
-    button3 = Button(window, text="3", padx=33, pady=22, font=("Arial", 15))
+    button1 = Button(window, text="1", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(1))
+    button2 = Button(window, text="2", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(2))
+    button3 = Button(window, text="3", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(3))
 
-    button4 = Button(window, text="4", padx=33, pady=22, font=("Arial", 15))
-    button5 = Button(window, text="5", padx=33, pady=22, font=("Arial", 15))
-    button6 = Button(window, text="6", padx=33, pady=22, font=("Arial", 15))
+    button4 = Button(window, text="4", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(4))
+    button5 = Button(window, text="5", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(5))
+    button6 = Button(window, text="6", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(6))
 
-    button7 = Button(window, text="7", padx=33, pady=22, font=("Arial", 15))
-    button8 = Button(window, text="8", padx=33, pady=22, font=("Arial", 15))
-    button9 = Button(window, text="9", padx=33, pady=22, font=("Arial", 15))
+    button7 = Button(window, text="7", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(7))
+    button8 = Button(window, text="8", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(8))
+    button9 = Button(window, text="9", padx=33, pady=22, font=("Arial", 15), command=lambda: value_return(9))
 
     button_decimal = Button(window, text=".", padx=35, pady=22, font=("Arial", 15))
-    button_clear = Button(window, text="A/C", padx=68, pady=22, font=("Arial", 15))
+    # button_clear calls clear_value function
+    button_clear = Button(window, text="A/C", padx=68, pady=22, font=("Arial", 15), command=clear_value)
     button_equals = Button(window, text="=", padx=79, pady=22, font=("Arial", 15))
 
     button_add = Button(window, text="+", padx=32, pady=22, font=("Arial", 15))
